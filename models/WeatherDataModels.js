0239077ebb6f775530231ed515ab4e414ea292c7
Martijn van Dijk <martijn.van.dijk.95@gmail.com>
2017-05-22 11:33:30 +0200

    added weather data model

---
+++ models/WeatherDataModels.js
@@ -0,0 +1,50 @@
+var mongoose = require('mongoose');
+var uuid = require('uuid/v4');
+
+var supportedMeasurements = ["temperature", "relativeHumidity"];
+
+var WeatherMeasurementSchema = mongoose.Schema({
+    sensorNodeID: {
+            type: String,
+            required:true
+        },
+    measurementType: {
+            type: String,
+            enum: supportedMeasurements,
+            required: true
+        },
+    value: {
+            type: Number,
+            required: true
+        },
+    timestamp: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+var SensorNodeSchema = mongoose.Schema({
+    name: {
+        type: String
+    },
+    sensorNodeID: {
+        type: String,
+        default: uuid(),
+        required: true
+    },
+    measurementTypes: {
+        type: [
+            {
+                type: String,
+                enum: supportedMeasurements
+            }
+        ]
+    },
+    dateAdded: {
+        type: Date,
+        default: Date.now()
+    }
+});
+
+module.exports.WeatherMeasurement = mongoose.model('WeatherMeasurement', WeatherMeasurementSchema);
+module.exports.SensorNode = mongoose.model('SensorNode', SensorNodeSchema);
